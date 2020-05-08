359c38606ac41ed30b1137073b6850c26db4fd65
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-08 21:16:23 +0930

    htmlRoutes created.

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const express = require ('express');
 // tells node that we are creating an express server
 const app = express();
 
+// sets an initial port. 
+const PORT = process.env.PORT || 5000;
+
 // handles data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // set static folder
-app.use (express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 
 // routers
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 
-// sets an initial port. 
-const PORT = process.env.PORT || 5000;
+
 
 // listener
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

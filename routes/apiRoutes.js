bf62907fe5086fad86d572b0a8f7af1001ee1514
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-08 22:20:28 +0930

    created app.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,19 @@ module.exports = function(app) {
         return res.json(notes);
     });
 
-  
+    app.post('/api/notes', (req, res ) => {
+        let newNote = req.body;
+        let id = notes.length;
+        newNote.id = id + 1;
+        notes.push(newNote);
+
+        fs.writeFileSync(path.join(__dirname, 'db/db.json'),
+        JSON.stringify(notes),
+        err => {
+            if (err) throw err;
+        })
+        return res.json(notes);
+    });
 
     app.delete('api/notes/:id', (req, res) => {
 

7cdc67ca24efe2eb71236e46ccb81dbf827d5ef6
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-11 13:03:49 +0930

    comments added.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// dependecies
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
 const db = require("./db/db.json");
 
+// sets an initial port 
 const PORT = process.env.PORT || 5000;
 
 const notes = JSON.parse(
@@ -22,9 +24,11 @@ const notesUpdate = (notes) => {
   );
 };
 
+// sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// css routes
 app.get("/assets/css/styles.css", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
 });
@@ -33,6 +37,7 @@ app.get("/assets/js/index.js", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
 });
 
+// html routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -41,10 +46,12 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// api routes
 app.get("/api/notes", (req, res) => {
   return res.json(notes);
 });
 
+// create new notes
 app.post("/api/notes", (req, res) => {
   let note = req.body;
   let id = notes.length;
@@ -54,6 +61,7 @@ app.post("/api/notes", (req, res) => {
   return res.json(notes);
 });
 
+// delete existing notes
 app.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
   let interval = 1;
@@ -66,4 +74,5 @@ app.delete("/api/notes/:id", (req, res) => {
   res.send(notes);
 });
 
+// Listener
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

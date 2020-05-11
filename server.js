74c60b73904bc4811bb2b347594f5c44e6aca84a
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-11 11:15:05 +0930

    updated server.js.

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-// dependencies
+const fs = require ('fs');
 const express = require ('express');
-// tells node that we are creating an express server
 const app = express();
 
-// sets an initial port. 
 const PORT = process.env.PORT || 5000;
 
-// handles data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// set static folder
-app.use(express.static("public"));
-app.use(express.static("db"));
-
-// routers
-// require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(express.static('public'));
+app.use(express.static('db'));
 
 
 
-// listener
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.listen (PORT, () => console.log(`Server started on port ${PORT}`));
